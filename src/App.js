652e9a7a6361e738f0847b9050f6c168766e91e6
ACER <mahanasty@mail.ru>
2021-07-19 17:37:39 +0300

    feat:add sgoppage with shopItem Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import {HomePage} from "./pages/homepage/HomePage";
 import {Route, Switch} from "react-router-dom";
+import ShopPage from "./pages/shoppage/ShopPage";
 
 function App() {
   return (
       <Switch>
-         <Route  path='/hats' component={HatsPage}/>
+         <Route  path='/shop' component={ShopPage}/>
          <Route exact path='/' component={HomePage}/>
       </Switch>
   );
@@ -14,11 +15,4 @@ function App() {
 
 export default App;
 
-export const HatsPage = () => {
-  return (
-      <div>
-        <h1>HatsComponents</h1>
-      </div>
-  )
 
-}
