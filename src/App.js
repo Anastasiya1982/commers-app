4320d91dbfd52c3fdebbb7f40500beb9bcc857d2
ACER <mahanasty@mail.ru>
2021-07-19 17:54:07 +0300

    feat:add header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import {HomePage} from "./pages/homepage/HomePage";
 import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shoppage/ShopPage";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-      <Switch>
-         <Route  path='/shop' component={ShopPage}/>
-         <Route exact path='/' component={HomePage}/>
-      </Switch>
-  );
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route path='/shop' component={ShopPage}/>
+                <Route exact path='/' component={HomePage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
